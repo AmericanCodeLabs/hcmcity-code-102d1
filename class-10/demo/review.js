1a40f15398dc35f7a0585eabaf80ba64afe1c80a
Khoan Gip <khoan@americancodelab.com>
2022-11-30 01:15:24 +0700

    Update review

--- class-10/demo/review.js
+++ class-10/demo/review.js
@@ -1,7 +1,32 @@
 // Link to my replit for review: 
 // https://replit.com/@KassieBradshaw/review102d45#index.js
 
+// function 2 ways
+
+// declaring  a variable called helloWorldA, assigning the value of a function that takes a name as a parameter
+// print a message using the name parameter to the console.
+let helloWorld = function (name) {
+  console.log('A new world awaits' + name);
+}
+
+// declaring a function that takes a name parameter
+// print a message using the name parameter to the console.
+function helloWorldB(name) {
+  console.log('A new world awaits' + name);
+}
+
+// expressions
+let x = 4 + 5;
+let name = "Khoan" + "Gip"
+
+// Assignment operators & comparison oparator
+let y = 5; // assigning the value of the number 5 to the variable y
+y += x;  // assigning the value of (y + x) to the variable y
+
 // WHILE LOOP
+// while(this condition evaluations to true){
+// this code will excute (run)
+//}
 // let usersName = prompt("What is your name?");
 
 // while (usersName !== "kassie"){
@@ -11,7 +36,11 @@
 // }
 
 // FOR LOOP
+// for (initial value; conditional expression; increment)
+// write your code here
 
-for (let i = 10; i >= 0; i = i-2){
+// for loop start at 5, initial value less than 50, increment by 5 each time
+
+for (let i = 10; i >= 0; i = i - 2) {
   console.log(i);
 }
